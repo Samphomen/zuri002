eb5ecad4fab2f814d1aa8ea0d8897891b85066ae
Samphomen <ishaqcole@gmail.com>
2023-09-13 17:04:50 +0100

    4th commit

--- db.js
+++ db.js
@@ -1,14 +1,30 @@
 const { Sequelize } = require("sequelize");
 require('dotenv').config()
 
+// const sequelize = new Sequelize({
+//     dialect: 'postgres',
+//     username: 'postgres',
+//     password: process.env.DB_PASSWORD,
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'zuri002',
+//   });
+
 const sequelize = new Sequelize({
-    dialect: 'postgres',
-    username: 'postgres',
-    password: process.env.DB_PASSWORD,
-    host: 'localhost',
-    port: 5432,
-    database: 'zuri002',
-  });
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  dialect: 'postgres',
+  pool: {
+    max: 100,
+    min: 0,
+    acquire: 10000,
+    idle: 20000
+  },
+  logging: false
+})
 
 
   const testDbConnection = async () => {
